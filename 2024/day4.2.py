a312bfb3380e2f2e5501ae81ccba40b318617f8f
Dmitrii Ulianov <107825352+DmitriiUlianov@users.noreply.github.com>
2024-12-08 18:30:24 +0100

    Create day4.2.py

---
+++ 2024/day4.2.py
@@ -0,0 +1,29 @@
+cont = ''
+filelen = 0
+with open("input.txt", "r") as file:
+    for line in file:
+        cont += '.' + line.strip()
+        strlen = len(line.strip()) + 1
+        filelen += strlen
+
+res = 0
+word1 = 'MAS'
+word2 = 'SAM'
+
+def func(word1, word2):
+    func_res = 0
+    for idx, i in enumerate(cont):
+        if cont[idx] == word1[0]:
+            if idx + 2 < filelen and cont[idx + 2] == word2[0]:
+                if idx + strlen + 1 < filelen and cont[idx + strlen + 1] == word1[1]:
+                    if idx + 2*strlen < filelen and cont[idx + 2*strlen] == word2[2]:
+                        if idx + 2*strlen + 2 < filelen and cont[idx + 2*strlen + 2] == word1[2]:
+                            func_res += 1
+    return func_res
+
+res += func(word1, word1)
+res += func(word1, word2)
+res += func(word2, word1)
+res += func(word2, word2)
+
+print(res)
