7b5d3e4056dd91cd41b33f299ec01836e9348361
Dmitrii Ulianov <107825352+DmitriiUlianov@users.noreply.github.com>
2024-12-19 23:44:18 +0300

    Create day_13.2.py

---
+++ 2024/day_13.2.py
@@ -0,0 +1,47 @@
+import re
+import numpy as np
+
+with open("input.txt", "r") as file:
+    comb = []
+    res = []
+    n = 0
+    for line in file:
+        parts = re.split(' |, |\n', line)
+        for item in parts:
+            if '+' in item or '=' in item:
+                if '+' in item:
+                    item = item.split('+')
+                elif '=' in item:
+                    item = item.split('=')
+                
+                n += 1
+                if n <= 6:
+                    res.append(int(item[1]))
+                if n == 6:
+                    comb.append(res)
+                    res = []
+                    n = 0
+
+total = 0
+n = str(10000000000000)
+for nums in comb:
+    a = np.array([[nums[0], nums[2]], [nums[1], nums[3]]])
+    b = np.array([ int(n + str(nums[4])), int(n + str(nums[5])) ])
+    x = np.linalg.solve(a, b)
+    print(x)
+    print(x[0])
+    print(x[1])
+    if (round(x[0],1) - round(x[0])) == 0 and (round(x[1],1) - round(x[1])) == 0:
+        total += 3*round(x[0]) + round(x[1])
+        
+print(total)# 4199821452475189258 too high
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
