539781abc1ad782e89783eaeab2aeb5e7a23ac2a
Dmitrii Ulianov <107825352+DmitriiUlianov@users.noreply.github.com>
2024-12-20 21:16:28 +0300

    Update day_13.2.py

--- 2024/day_13.2.py
+++ 2024/day_13.2.py
@@ -23,20 +23,12 @@ with open("input.txt", "r") as file:
                     n = 0
 
 total = 0
-n = str(10000000000000)
+n = 10000000000000
 for nums in comb:
     a = np.array([[nums[0], nums[2]], [nums[1], nums[3]]])
-    b = np.array([ int(n + str(nums[4])), int(n + str(nums[5])) ])
+    b = np.array([n + nums[4], n + nums[5]])
     x = np.linalg.solve(a, b)
-    print(x)
-    print(x[0])
-    print(x[1])
-    print(round(x[0],1))
-    print(round(x[1],1))
     if (round(x[0],1) - round(x[0])) == 0 and (round(x[1],1) - round(x[1])) == 0:
         total += 3*round(x[0]) + round(x[1])
-        print(1)
-    else:
-        print(0)
         
-print(total) # 4199821452475189258 too high
+print(total)
