a77b6dd032f4d214f7a523f74e87559b739ed019
Dmitrii Ulianov <107825352+DmitriiUlianov@users.noreply.github.com>
2024-12-26 20:22:34 +0300

    Update day9.1.py

--- 2024/day9.1.py
+++ 2024/day9.1.py
@@ -17,32 +17,17 @@ for idx, i in enumerate(nums):
         while i > 0:
             data.append('.')
             i -= 1
-print(data)
-
 
 while '.' in data:
     idx = data.index('.')
+    data[idx],data[-1] = data[-1],data[idx]
+    data.pop(-1)
     l = len(data)
     while data[-1] == '.':
-        #data = data[:l-1]
         data.pop(-1)
         l -= 1
-    '''   
-    new_data = []
-    new_data.append(data[0:idx])
-    new_data.append(data[l - 1])
-    new_data.append(data[idx + 1:l - 1])
-    data = new_data.copy()
-    '''
-    #print(data[idx])
-    #print(data[-1])
-    if '.' in data:
-        data[idx],data[-1] = data[-1],data[idx]
-        data.pop(-1)
-        print(data)
-print(data)
-
+        
 res = 0
 for idx, i in enumerate(data):
-    res += int(i)*idx
-print(res) #91381988682 to low
+    res += i*idx
+print(res)
